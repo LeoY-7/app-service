a473852b6d76d9523a3c661b75b17f4e532a23d0
Leo <leoy730@gmail.com>
2020-08-09 23:55:23 +0800

    0809 effort

--- parent/common/src/main/java/leo/util/StringTools.java
+++ parent/common/src/main/java/leo/util/StringTools.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 public class StringTools {
 
     public void dataMask(HashMap<String, String> map) {
-        for (String key:map.keySet()) {
+        for (String key:map.keySet()
+             ) {
             if ("base_acct_no".equals(key)) {
                 map.put(key, "Masked:" + map.get(key));
             }
